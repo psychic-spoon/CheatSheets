a5b14a156723d83c7bfa54f562bb11e96c4be830
ASHISHPANI988 <you@example.com>
2017-11-14 08:22:11 +0530

    Flask update Mysql

--- Flask/MyFlaskApp/app.py
+++ Flask/MyFlaskApp/app.py
@@ -1,7 +1,24 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,flash,redirect,url_for,session, logging, request
 from data import Articles
+from flask_mysqldb import MySQL
+from wtforms import Form, StringField, TextAreaField, PasswordField, validators
+from passlib.hash import sha256_crypt
+
 
 app=Flask(__name__)    #__name__ is placeholder for current module( app.py)
+
+app.secret_key='secret123'
+
+#Config MySQL
+app.config['MYSQL_HOST']='localhost'
+app.config['MYSQL_USER']='ashish'
+app.config['MYSQL_PASSWORD']='ashish'
+app.config['MYSQL_DB']='flaskapp'
+app.config['MYSQL_CURSORCLASS']='DictCursor'
+
+#init MySQL
+mysql=MySQL(app)
+
 
 Articles=Articles()
 
@@ -20,6 +37,39 @@ def articles():
 @app.route('/article/<string:id>')
 def article(id):
     return render_template('article.html',id=id)
+
+
+class RegisterForm(Form):
+	name 		 = StringField('Name',[validators.Length(min=1,max=20)])
+	username     = StringField('Username', [validators.Length(min=4, max=25)])
+	email        = StringField('Email Address', [validators.Length(min=6, max=35),validators.Required()])
+	password     = PasswordField('Password',[validators.DataRequired(),validators.EqualTo('confirm',message='Password don not match')])
+	confirm		 = PasswordField('Confirm Password')
+
+@app.route('/register',methods=['GET','POST'])
+def register():
+	form = RegisterForm(request.form)
+	if request.method == 'POST' and form.validate():
+		name=form.name.data
+		email=form.email.data
+		username=form.username.data
+		password=sha256_crypt.encrypt(str(form.password.data))
+
+		#Create cursor
+		cur=mysql.connection.cursor()
+
+		cur.execute("insert into users(name,email,username,password) values(%s,%s,%s,%s)",(name,email,username,password))		
+		
+		#Commit to DB
+		mysql.connection.commit()
+
+		#Close connection
+		cur.close()
+		flash("You are now registered and can log in",'success')
+		#redirect_url()
+		redirect('/index')
+	return render_template('register.html',form=form)
+
 
 if __name__=='__main__':
 	app.run(debug=True)
---
+++ R/FirstShinyApp/FirstShiny/ui.R
@@ -0,0 +1,34 @@
+#
+# This is the user-interface definition of a Shiny web application. You can
+# run the application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+# 
+#    http://shiny.rstudio.com/
+#
+library(shiny)
+
+# Define UI for application that draws a histogram
+shinyUI(fluidPage(
+  # Application title
+  titlePanel("Old Faithful Geyser Data"),
+  tags$h1("Something"),
+  numericInput(inputId="n","Input n",value=13,min = 1,max=15),
+  textOutput(outputId="nx"),
+  plotOutput(outputId="histPlot"),
+  # Sidebar with a slider input for number of bins 
+  sidebarLayout(
+    sidebarPanel(
+       sliderInput("bins",
+                   "Number of bins:",
+                   min = 1,
+                   max = 50,
+                   value = 30)
+    ),
+    
+    # Show a plot of the generated distribution
+    mainPanel(
+       plotOutput("distPlot")
+    )
+  )
+))
