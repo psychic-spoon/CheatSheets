49ef35bbc456e68438d1691ffae9cab7e77c2b8d
psychic-spoon <ashishpani988@gmail.com>
2018-11-05 21:22:16 +0530

    Send Email

---
+++ Python/Email.py
@@ -0,0 +1,112 @@
+# https://medium.freecodecamp.org/send-emails-using-code-4fcea9df63f
+import smtplib
+from email.MIMEMultipart import MIMEMultipart
+from email.MIMEText import MIMEText
+from email.MIMEBase import MIMEBase
+from email import encoders
+ 
+fromaddr = "YOUR EMAIL"
+toaddr = "EMAIL ADDRESS YOU SEND TO"
+ 
+msg = MIMEMultipart()
+ 
+msg['From'] = fromaddr
+msg['To'] = toaddr
+msg['Subject'] = "SUBJECT OF THE EMAIL"
+ 
+body = "TEXT YOU WANT TO SEND"
+ 
+msg.attach(MIMEText(body, 'plain'))
+ 
+filename = "NAME OF THE FILE WITH ITS EXTENSION"
+attachment = open("PATH OF THE FILE", "rb")
+ 
+part = MIMEBase('application', 'octet-stream')
+part.set_payload((attachment).read())
+encoders.encode_base64(part)
+part.add_header('Content-Disposition', "attachment; filename= %s" % filename)
+ 
+msg.attach(part)
+ 
+server = smtplib.SMTP('smtp.gmail.com', 587)
+server.starttls()
+server.login(fromaddr, "YOUR PASSWORD")
+text = msg.as_string()
+server.sendmail(fromaddr, toaddr, text)
+server.quit()
+
+MY_ADDRESS = 'my_address@example.comm'
+PASSWORD = 'mypassword'
+
+def get_contacts(filename):
+    """
+    Return two lists names, emails containing names and email addresses
+    read from a file specified by filename.
+    """
+    
+    names = []
+    emails = []
+    with open(filename, mode='r', encoding='utf-8') as contacts_file:
+        for a_contact in contacts_file:
+            names.append(a_contact.split()[0])
+            emails.append(a_contact.split()[1])
+    return names, emails
+
+def read_template(filename):
+    """
+    Returns a Template object comprising the contents of the 
+    file specified by filename.
+    """
+    
+    with open(filename, 'r', encoding='utf-8') as template_file:
+        template_file_content = template_file.read()
+    return Template(template_file_content)
+
+def main():
+    names, emails = get_contacts('mycontacts.txt') # read contacts
+    message_template = read_template('message.txt')
+
+    # set up the SMTP server
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.starttls()
+    server.login(fromaddr, "YOUR PASSWORD")
+
+    # For each contact, send the email:
+    for name, email in zip(names, emails):
+        msg = MIMEMultipart()       # create a message
+
+        # add in the actual person name to the message template
+        message = message_template.substitute(PERSON_NAME=name.title())
+
+        # Prints out the message body for our sake
+        print(message)
+
+        # setup the parameters of the message
+        msg['From']=MY_ADDRESS
+        msg['To']=email
+        msg['Subject']="This is TEST"
+        
+        # add in the message body
+        msg.attach(MIMEText(message, 'plain'))
+        
+        filename = "NAME OF THE FILE WITH ITS EXTENSION"
+        attachment = open("PATH OF THE FILE", "rb")
+        
+        part = MIMEBase('application', 'octet-stream')
+        part.set_payload((attachment).read())
+        encoders.encode_base64(part)
+        part.add_header('Content-Disposition', "attachment; filename= %s" % filename)
+        
+        msg.attach(part)
+
+        # send the message via the server set up earlier.
+        # server.send_message(msg)
+        # del msg
+        text = msg.as_string()
+        server.sendmail(fromaddr, toaddr, text)
+
+    # Terminate the SMTP session and close the connection
+    server.quit()
+    
+if __name__ == '__main__':
+    main()
