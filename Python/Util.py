a1d3a7e98c71ad400221608e9b22e3d9a58a6106
psychic-spoon <ashishpani988@gmail.com>
2018-11-05 23:20:48 +0530

    Utility functions

---
+++ Python/Util.py
@@ -0,0 +1,47 @@
+import time
+
+s="psychic-spoon"
+
+'''Reversing a String'''
+# start=time.time()
+# time.sleep(2)
+# print(s[::-1])
+# elapsed=time.time()-start
+# print("Slice Method:"+str(elapsed))
+
+# start=time.time()
+# time.sleep(2)
+# print(''.join(reversed(s)))
+# elapsed=time.time()-start
+# print("Reversed Method:"+str(elapsed))
+
+# start=time.time()
+# time.sleep(2)
+# chars=list(s)
+# length=len(s)
+# for i in range(int(length/2)):
+#     chars[i],chars[length-1-i]=chars[length-1-i],chars[i]
+# s=''.join(chars)
+# print(s)    
+# elapsed=time.time()-start
+# print("Inplace Method:"+str(elapsed))
+
+
+'''Filter null strings'''
+# s='A B  C'
+
+# start=time.time()
+# time.sleep(1)
+# lst=list(filter(None,s.split(' ')))
+# elapsed=time.time()-start
+# print("List Filter:"+str(elapsed))
+# print(str(lst))
+
+# start=time.time()
+# time.sleep(1)
+# lst=[x for x in s.split(' ') if x]
+# elapsed=time.time()-start
+# print("List If Filter:"+str(elapsed))
+# print(str(lst))
+
+'''Reversing a list'''
